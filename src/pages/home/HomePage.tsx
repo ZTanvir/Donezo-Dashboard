241220706a5c39d80c5bd85c1afe91168536d840
zahirul <zahirulopel@gmail.com>
2026-02-26 17:30:11 +0600

    add details login section for desktop

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -3,15 +3,15 @@ import { FiTarget } from "react-icons/fi";
 const HomePage = () => {
   return (
     <div>
-      <div>
+      <div className="grid space-x-2 md:grid-cols-2">
         <section id="loginForm">
           <div className="px-4 py-4">
             <div className="mb-6 flex items-center justify-center space-x-3 md:mb-8 md:justify-start">
               <FiTarget className="text-green-950" size={40} />
               <span className="text-2xl font-bold text-gray-900">Donezo</span>
             </div>
-            <div className="mx-auto max-w-2xl">
-              <h1 className="mb-2 text-center text-4xl font-bold text-gray-950 md:text-left">
+            <div className="mx-auto max-w-lg">
+              <h1 className="mb:text-4xl mb-2 text-center text-2xl font-bold text-gray-950 md:text-left">
                 Welcome back
               </h1>
               <p className="mb-4 text-center text-gray-500 md:text-left">
@@ -36,9 +36,6 @@ const HomePage = () => {
                     placeholder="password123"
                   />
                 </div>
-                <p className="mb-4 text-right text-sm font-semibold text-green-800">
-                  Forgot password?
-                </p>
                 <button
                   className="w-full rounded-2xl bg-green-900 py-3 font-bold text-white transition duration-150 hover:cursor-pointer hover:bg-green-800"
                   type="submit"
@@ -49,7 +46,19 @@ const HomePage = () => {
             </div>
           </div>
         </section>
-        <section id="details"></section>
+        <section id="details" className="hidden md:block">
+          <div className="flex h-screen flex-col items-center justify-center rounded-2xl bg-radial-[at_80%_20%] from-green-800 to-green-950">
+            <div className="max-w-xl px-10">
+              <h2 className="mb-8 text-center text-3xl font-bold text-white">
+                Plan, prioritize, and accomplish your tasks with ease.
+              </h2>
+              <p className="text-center text-white">
+                Join thousands of teams already using Donezo to manage their
+                daily workflows and projects effectively.
+              </p>
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   );
