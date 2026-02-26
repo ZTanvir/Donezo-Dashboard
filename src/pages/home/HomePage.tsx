bd38d831783a20bd39aadce9650cc48069c6bc68
zahirul <zahirulopel@gmail.com>
2026-02-26 16:39:02 +0600

    add login form to login page

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,7 +1,56 @@
+import { FiTarget } from "react-icons/fi";
+
 const HomePage = () => {
   return (
     <div>
-      <h1 className="text-xl">Home page</h1>
+      <div>
+        <section id="loginForm">
+          <div className="px-4 py-4">
+            <div className="mb-6 flex items-center justify-center space-x-3 md:mb-8 md:justify-start">
+              <FiTarget className="text-green-950" size={40} />
+              <span className="text-2xl font-bold text-gray-900">Donezo</span>
+            </div>
+            <div className="mx-auto max-w-2xl">
+              <h1 className="mb-2 text-center text-4xl font-bold text-gray-950 md:text-left">
+                Welcome back
+              </h1>
+              <p className="mb-4 text-center text-gray-500 md:text-left">
+                Please enter your details to sign in.
+              </p>
+              <form className="">
+                <div className="mb-4 flex flex-col space-y-2">
+                  <label htmlFor="email">Email Address</label>
+                  <input
+                    className="rounded-xl border border-gray-300 px-3 py-2 text-gray-700 transition-all duration-150 focus:ring-gray-500"
+                    type="text"
+                    id="email"
+                    placeholder="user1@example.com"
+                  />
+                </div>
+                <div className="mb-4 flex flex-col space-y-2">
+                  <label htmlFor="password">Password</label>
+                  <input
+                    className="rounded-xl border border-gray-300 px-3 py-2 text-gray-700 transition-all duration-150 focus:ring-gray-500"
+                    type="password"
+                    id="password"
+                    placeholder="password123"
+                  />
+                </div>
+                <p className="mb-4 text-right text-sm font-semibold text-green-800">
+                  Forgot password?
+                </p>
+                <button
+                  className="w-full rounded-2xl bg-green-900 py-3 font-bold text-white transition duration-150 hover:cursor-pointer hover:bg-green-800"
+                  type="submit"
+                >
+                  Sign in
+                </button>
+              </form>
+            </div>
+          </div>
+        </section>
+        <section id="details"></section>
+      </div>
     </div>
   );
 };
