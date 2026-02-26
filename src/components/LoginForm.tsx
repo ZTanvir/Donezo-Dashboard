47b286b18dcab8f18e5721bc0a2ab844eb2ddb58
zahirul <zahirulopel@gmail.com>
2026-02-26 19:35:07 +0600

    add icons to login form input fields

---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import { MdOutlineEmail } from "react-icons/md";
+import { CiLock } from "react-icons/ci";
+import { FaRegEyeSlash } from "react-icons/fa";
+
+const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <form className="">
+      <div className="mb-4 flex flex-col space-y-2">
+        <label htmlFor="email">Email Address</label>
+        <div className="relative">
+          <MdOutlineEmail
+            className="absolute top-1/2 left-3 -translate-y-1/2 text-gray-400"
+            size={20}
+          />
+          <input
+            className="w-full rounded-xl border border-gray-300 py-2 pr-3 pl-10 text-gray-700 transition-all duration-150 focus:ring-gray-500"
+            type="text"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="user1@example.com"
+          />
+        </div>
+      </div>
+      <div className="mb-4 flex flex-col space-y-2">
+        <label htmlFor="password">Password</label>
+        <div className="relative">
+          <CiLock
+            className="absolute top-1/2 left-3 -translate-y-1/2 text-gray-400"
+            size={20}
+          />
+          <input
+            className="w-full rounded-xl border border-gray-300 px-10 py-2 text-gray-700 transition-all duration-150 focus:ring-gray-500"
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="password123"
+          />
+          <FaRegEyeSlash
+            size={20}
+            className="absolute top-1/2 right-3 -translate-y-1/2 text-gray-400 hover:cursor-pointer"
+          />
+        </div>
+      </div>
+      <button
+        className="w-full rounded-2xl bg-green-900 py-3 font-bold text-white transition duration-150 hover:cursor-pointer hover:bg-green-800"
+        type="submit"
+      >
+        Sign in
+      </button>
+    </form>
+  );
+};
+export default LoginForm;
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,53 +1,29 @@
 import { FiTarget } from "react-icons/fi";
+import LoginForm from "../../components/LoginForm";
 
 const HomePage = () => {
   return (
     <div>
-      <div className="grid space-x-2 md:grid-cols-2">
+      <div className="grid space-x-2 px-4 py-4 lg:grid-cols-2">
         <section id="loginForm">
-          <div className="px-4 py-4">
+          <div>
             <div className="mb-6 flex items-center justify-center space-x-3 md:mb-8 md:justify-start">
               <FiTarget className="text-green-950" size={40} />
               <span className="text-2xl font-bold text-gray-900">Donezo</span>
             </div>
             <div className="mx-auto max-w-lg">
-              <h1 className="mb:text-4xl mb-2 text-center text-2xl font-bold text-gray-950 md:text-left">
+              <h1 className="mb-2 text-center text-2xl font-bold text-gray-950 md:text-left md:text-4xl">
                 Welcome back
               </h1>
               <p className="mb-4 text-center text-gray-500 md:text-left">
                 Please enter your details to sign in.
               </p>
-              <form className="">
-                <div className="mb-4 flex flex-col space-y-2">
-                  <label htmlFor="email">Email Address</label>
-                  <input
-                    className="rounded-xl border border-gray-300 px-3 py-2 text-gray-700 transition-all duration-150 focus:ring-gray-500"
-                    type="text"
-                    id="email"
-                    placeholder="user1@example.com"
-                  />
-                </div>
-                <div className="mb-4 flex flex-col space-y-2">
-                  <label htmlFor="password">Password</label>
-                  <input
-                    className="rounded-xl border border-gray-300 px-3 py-2 text-gray-700 transition-all duration-150 focus:ring-gray-500"
-                    type="password"
-                    id="password"
-                    placeholder="password123"
-                  />
-                </div>
-                <button
-                  className="w-full rounded-2xl bg-green-900 py-3 font-bold text-white transition duration-150 hover:cursor-pointer hover:bg-green-800"
-                  type="submit"
-                >
-                  Sign in
-                </button>
-              </form>
+              <LoginForm />
             </div>
           </div>
         </section>
-        <section id="details" className="hidden md:block">
-          <div className="flex h-screen flex-col items-center justify-center rounded-2xl bg-radial-[at_80%_20%] from-green-800 to-green-950">
+        <section id="details" className="hidden lg:block">
+          <div className="flex h-full flex-col items-center justify-center rounded-2xl bg-radial-[at_80%_20%] from-green-800 to-green-950">
             <div className="max-w-xl px-10">
               <h2 className="mb-8 text-center text-3xl font-bold text-white">
                 Plan, prioritize, and accomplish your tasks with ease.
