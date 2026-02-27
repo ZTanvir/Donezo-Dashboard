e2655e9696f3037fb521bb8d2e54a7373d61b539
zahirul <zahirulopel@gmail.com>
2026-02-27 16:10:31 +0600

    add dashboard page

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import HomePage from "./pages/home/HomePage";
 import { BrowserRouter, Routes, Route } from "react-router";
 import UserContextLayout from "./layouts/UserContextLayout";
 import ProtectedUserRoute from "./lib/ProtectedUserRoute";
+import Dashboard from "./pages/dashboard/Dashboard";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -13,7 +14,7 @@ createRoot(document.getElementById("root")!).render(
         <Route element={<UserContextLayout />}>
           <Route path="/" element={<HomePage />} />
           <Route element={<ProtectedUserRoute />}>
-            <Route path="/dashboard" element={<div>Dashboard</div>} />
+            <Route path="/dashboard" element={<Dashboard />} />
           </Route>
         </Route>
       </Routes>
